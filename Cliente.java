c7382ef88dc5bdbfb150f1aaf726a67e0d9cf18a
ElFalsoLicendiado <chopytrollface@gmail.com>
2024-03-12 09:59:28 -0600

    Fracaso

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+
 public class Cliente {
+
+    ArrayList <Pedido> orden = new ArrayList<Pedido>();
+    
+    String nombre = "";
+
+    
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void ordenarPlatillo(){
+        
+    }
+
     
 }
