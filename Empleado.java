eb4db1072869142c1ffb73a59f4b6f65a9cea7c8
ElFalsoLicendiado <chopytrollface@gmail.com>
2024-03-12 09:42:53 -0600

    Luis Chamba

--- Empleado.java
+++ Empleado.java
@@ -1,6 +1,20 @@
+import java.util.Random;
+
 public class Empleado {
+    static int numEmpleados = 0;
+    Random r = new Random();
+
+
     String name = "";
     int age = 0;
+    int id = 0;
     
+
+    public Empleado(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = r.nextInt(9999, 10000);
+    }
     
+
 }
